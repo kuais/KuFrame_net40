803b2e04d3a58c302e44ca6c8ba87549f0bdf5b2
kuais <dlm321@126.com>
2020-09-11 12:43:14 +0800

    200911

--- KuFrame v1.0.0.0/io/serial/KuSerial.cs
+++ KuFrame v1.0.0.0/io/serial/KuSerial.cs
@@ -1,36 +1,96 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
 
-namespace Ku.io
+namespace Ku.io.serial
 {
-    public class KuSerial : SerialPort, IOHandler
+    public class KuSerial
     {
-        public KuBuffer recvBuffer = new KuBuffer();
-        long timeActivate = 0;
-        public KuSerial() : base()
+        protected int _receivedMax = 1024;              //单次接收数据上限
+        protected long _timeActived = 0;
+        protected long _timeoutActived = 5000;
+        protected Exception _lastError;
+        protected SerialPort _sp;
+        protected KuThread _rRead = new KuThread();
+
+        #region 
+        public int Baudrate { get; set; } = 9600;
+        public string Port { get; set; } = "";
+        public bool IsOpened { get; private set; } = false;
+        public bool IsActivated => (_timeActived + _timeoutActived * 10000) > DateTime.Now.Ticks;
+        public Exception LastError => _lastError;
+        public KuBuffer Buffer { get; protected set; } = new KuBuffer();
+        public Stream Stream => _sp.BaseStream;
+        public ISerialListener SerialListener { get; set; }
+        #endregion
+
+        public void Open()
         {
+            Close();
+            _sp = new SerialPort(Port, Baudrate, 0);
+            _rRead.Loop(TaskRead, 100);
+            IsOpened = true;
         }
-        public KuSerial(string portName) : base(portName)
+        public void Open(string port)
         {
+            Port = port;
+            Open();
+        }
+        public void Open(string port, int baudrate)
+        {
+            Baudrate = baudrate;
+            Open(port);
+        }
+        public void Close()
+        {
+            _rRead.WaitStop();
+            if (_sp != null) _sp.Close();
+            _sp = null;
+            IsOpened = false;
+        }
+        public void Write(byte[] datas)
+        {
+            _sp.Write(datas, 0, datas.Length);
+            _timeActived = DateTime.Now.Ticks;
+            OnWrote(datas);
+        }
+        protected void OnRead(byte[] datas)
+        {
+            if (SerialListener != null) SerialListener.OnRead(datas);
+        }
+        protected void OnWrote(byte[] datas)
+        {
+            if (SerialListener != null) SerialListener.OnWrote(datas);
+        }
+        protected void OnError(Exception ex)
+        {
+            _lastError = ex;
+            if (SerialListener != null) SerialListener.OnError(ex);
         }
 
-        public byte[] Read()
+        private void TaskRead()
         {
-            if (BytesToRead <= 0) return new byte[0];
-            timeActivate = DateTime.Now.Ticks;
-            byte[] temp = new byte[BytesToRead];
-            Read(temp, 0, temp.Length);
-            recvBuffer.Put(temp);
-            return temp;
+            try
+            {
+                if (_sp.BytesToRead <= 0) return;
+                _timeActived = DateTime.Now.Ticks;
+                byte[] b = new byte[_receivedMax];
+                int len = _sp.Read(b, 0, b.Length);
+                byte[] datas = new byte[len];
+                System.Buffer.BlockCopy(b, 0, datas, 0, len);
+                Buffer.Put(datas);
+                OnRead(datas);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex);
+            }
         }
-        public bool Write(byte[] temp)
+
+        public interface ISerialListener : IError
         {
-            Write(temp, 0, temp.Length);
-            return true;
-        }
-        public bool CheckActivated(long ms)
-        {
-            return DateTime.Now.Ticks < (timeActivate + ms * 10000);
+            void OnRead(byte[] data);
+            void OnWrote(byte[] data);
         }
     }
 }
